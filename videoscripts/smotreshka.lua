18098c0388c7ae2a6b0f516481f7821a52850f04
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-11 19:28:08 +0300

    Update smotreshka.lua

--- videoscripts/smotreshka.lua
+++ videoscripts/smotreshka.lua
@@ -1,9 +1,12 @@
--- видеоскрипт для плейлиста "Смотрёшка" https://smotreshka.tv (15/9/20)
--- Copyright © 2017-2020 Nexterr
--- необходим скрапер TVS: smotreshka_pls
--- логин, пароль установить в 'Password Manager', для id - smotreshka
--- открывает подобные ссылки:
+-- видеоскрипт для плейлиста "Смотрёшка" https://smotreshka.tv (11/10/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
+-- ## необходим ##
+-- расширение дополнения httptimeshift: smotreshka-timeshift_ext.lua
+-- скрапер TVS: smotreshka_pls.lua
+-- ## логин, пароль установить в 'Password Manager', для id - smotreshka ##
+-- ## открывает подобные ссылки ##
 -- https://smotreshka.tv/5aead81921887f04724d1780
+-- https://smotreshka.tv/5dff7836b7ee6bd2ad034530
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://smotreshka%.tv/%x') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -87,54 +90,77 @@
 		if not tab
 			or not tab.languages
 			or not tab.languages[1].renditions[1]
-			or not tab.languages[1].renditions[1].url
 		then
 			m_simpleTV.User.smotreshka = nil
 			showError('4')
 		 return
 		end
-	local retAdr = tab.languages[1].renditions[1].url:gsub('u0026', '&')
-	rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
-	m_simpleTV.Http.Close(session)
-		if rc ~= 200 then return end
-	local base = retAdr:match('.+/')
 	local t, i = {}, 1
-		for res, br, res1, adr in answer:gmatch('EXT%-X%-STREAM%-IN([%C]+)[:,]BANDWIDTH=(%d+)([%C]*).-\n(.-)\n') do
-			t[i] = {}
-			br = tonumber(br)
-			br = math.ceil(br / 10000) * 10
-			res = res:match('RESOLUTION=(%d+x%d+)')
-				or res1:match('RESOLUTION=(%d+x%d+)')
-			if res then
-				t[i].Name = res .. ' (' .. br .. ' кбит/с)'
-				res = res:match('x(%d+)')
-				t[i].Id = tonumber(res)
-			else
-				t[i].Name = 'аудио (' .. br .. ' кбит/с)'
-				t[i].Id = 0
+	local lastQuality, auto
+	if tab.languages[1].renditions[1].id ~= 'Auto' then
+		local caption
+		auto = false
+			while tab.languages[1].renditions[i] do
+				t[i] = {}
+				caption = tab.languages[1].renditions[i].caption
+				t[i].Name = caption
+				caption = caption:gsub('Max4k', '2260')
+				caption = caption:match('%d+') or 0
+				t[i].Id = tonumber(caption)
+				t[i].Address = tab.languages[1].renditions[i].url:gsub('u0026', '&')
+				i = i + 1
 			end
-			if not adr:match('^%s*http') then
-				adr = base .. adr:gsub('^[%s/%.]+', '')
+			if #t == 0 then
+				showError('5')
+			 return
 			end
-			t[i].Address = adr
-			i = i + 1
-		end
-		if i == 1 then
-			m_simpleTV.Control.CurrentAddress = retAdr
-		 return
-		end
+		lastQuality = tonumber(m_simpleTV.Config.GetValue('smotreshka_qlty_noAuto')) or 5000
+	else
+		local retAdr = tab.languages[1].renditions[1].url:gsub('u0026', '&')
+		rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
+		m_simpleTV.Http.Close(session)
+			if rc ~= 200 then return end
+		local base = retAdr:match('.+/')
+		auto = true
+			for res, br, res1, adr in answer:gmatch('EXT%-X%-STREAM%-IN([%C]+)[:,]BANDWIDTH=(%d+)([%C]*).-\n(.-)\n') do
+				t[i] = {}
+				br = tonumber(br)
+				br = math.ceil(br / 10000) * 10
+				res = res:match('RESOLUTION=(%d+x%d+)')
+					or res1:match('RESOLUTION=(%d+x%d+)')
+				if res then
+					t[i].Name = res .. ' (' .. br .. ' кбит/с)'
+					res = res:match('x(%d+)')
+					t[i].Id = tonumber(res)
+				else
+					t[i].Name = 'аудио (' .. br .. ' кбит/с)'
+					t[i].Id = 0
+				end
+				if not adr:match('^%s*http') then
+					adr = base .. adr:gsub('^[%s/%.]+', '')
+				end
+				t[i].Address = adr
+				i = i + 1
+			end
+			if #t == 0 then
+				m_simpleTV.Control.CurrentAddress = retAdr
+			 return
+			end
+		lastQuality = tonumber(m_simpleTV.Config.GetValue('smotreshka_qlty')) or 5000
+	end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
-	local lastQuality = tonumber(m_simpleTV.Config.GetValue('smotreshka_qlty')) or 5000
 	local index = #t
 	if #t > 1 then
-		t[#t + 1] = {}
-		t[#t].Id = 5000
-		t[#t].Name = '▫ всегда высокое'
-		t[#t].Address = t[#t - 1].Address
-		t[#t + 1] = {}
-		t[#t].Id = 10000
-		t[#t].Name = '▫ адаптивное'
-		t[#t].Address = retAdr
+		if auto == true then
+			t[#t + 1] = {}
+			t[#t].Id = 5000
+			t[#t].Name = '▫ всегда высокое'
+			t[#t].Address = t[#t - 1].Address
+			t[#t + 1] = {}
+			t[#t].Id = 10000
+			t[#t].Name = '▫ адаптивное'
+			t[#t].Address = retAdr
+		end
 		index = #t
 			for i = 1, #t do
 				if t[i].Id >= lastQuality then
@@ -152,7 +178,11 @@
 				t.OkButton = {ButtonImageCx = 30, ButtonImageCy= 30, ButtonImage = m_simpleTV.User.paramScriptForSkin_buttonOk}
 			end
 			t.ExtButton1 = {ButtonEnable = true, ButtonName = '✕', ButtonScript = 'm_simpleTV.Control.ExecuteAction(37)'}
-			t.ExtParams = {LuaOnOkFunName = 'smotreshkaSaveQuality'}
+			if auto == true then
+				t.ExtParams = {LuaOnOkFunName = 'smotreshkaSaveQuality'}
+			else
+				t.ExtParams = {LuaOnOkFunName = 'smotreshkaSaveQuality_noAuto'}
+			end
 			m_simpleTV.OSD.ShowSelect_UTF8('⚙ Качество', index - 1, t, 5000, 32 + 64 + 128)
 		end
 	end
@@ -160,6 +190,11 @@
 	function smotreshkaSaveQuality(obj, id)
 		if id > 0 then
 			m_simpleTV.Config.SetValue('smotreshka_qlty', id)
+		end
+	end
+	function smotreshkaSaveQuality_noAuto(obj, id)
+		if id > 0 then
+			m_simpleTV.Config.SetValue('smotreshka_qlty_noAuto', id)
 		end
 	end
 -- debug_in_file(t[index].Address .. '\n')
