8af8bac5d300df6b9311b5f9030a3b3679698c84
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-13 12:01:26 +0300

    Update zonamobi.lua

--- videoscripts/zonamobi.lua
+++ videoscripts/zonamobi.lua
@@ -1,24 +1,27 @@
--- видеоскрипт для сайта https://w6.zona.plus (18/4/20)
+-- видеоскрипт для сайта https://w1.zona.plus (14/10/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 -- открывает подобные ссылки:
--- https://w6.zona.plus/movies/hanter-killer
+-- https://w100.zona.plus/movies/hanter-killer
 -- https://w6.zona.plus/tvseries/vikingi-2013/season-1
 -------------------------------------------------------------------------
 local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 -------------------------------------------------------------------------
-		if m_simpleTV.Control.ChangeAdress ~= 'No' then return end
-	local inAdr = m_simpleTV.Control.CurrentAdress
-		if not inAdr then return end
-		if not inAdr:match('https?://[%w%.]*zona%.plus') then return end
+		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://[%w%.]*zona%.plus') then return end
+	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.OSD.ShowMessageT({text = '', showTime = 1000, id = "channelName"})
 	if inAdr:match('zona%.plus') and not inAdr:match('&kinopoisk') then
-		m_simpleTV.Interface.SetBackground({BackColor = 0, BackColorEnd = 255, PictFileName = '', TypeBackColor = 0, UseLogo = 3, Once = 1})
+		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
+	end
+	local function showError(str)
+		m_simpleTV.OSD.ShowMessageT({text = 'zona.plus ошибка: ' .. str, showTime = 5000, color = 0xffff1000, id = 'channelName'})
 	end
 	inAdr = inAdr:gsub('&kinopoisk', '')
-	m_simpleTV.Control.ChangeAdress = 'Yes'
-	m_simpleTV.Control.CurrentAdress = ''
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.2785.143 Safari/537.36')
+	m_simpleTV.Control.ChangeAddress = 'Yes'
+	m_simpleTV.Control.CurrentAddress = ''
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (iPhone; CPU OS 10_15_5 (Ergänzendes Update) like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/14E304 Safari/605.1.15')
 		if not session then return end
-	m_simpleTV.Http.SetTimeout(session, 8000)
+	m_simpleTV.Http.SetTimeout(session, 16000)
 	local function unescape_html(str)
 		str = str:gsub(' смотреть онлайн.+', '')
 		str = str:gsub('—', '-')
@@ -48,7 +51,7 @@ local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
 		if rc ~= 200 then
 			m_simpleTV.Http.Close(session)
-			m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[1]-' .. rc, 255, 5)
+			showError('1')
 		 return
 	end
 	local title = answer:match('itemprop="name">(.-)</span>') or 'zonamobi'
@@ -62,13 +65,13 @@ local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 		retAdr = answer:match('"video".-data%-id="(.-)"')
 			if not retAdr then
 				m_simpleTV.Http.Close(session)
-				m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[2]', 255, 5)
+				showError('2')
 			 return
 			end
 		rc, answer = m_simpleTV.Http.Request(session, {url = host .. '/api/v1/video/' .. retAdr})
 			if rc ~= 200 then
 				m_simpleTV.Http.Close(session)
-				m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[2.1]-' .. rc, 255, 5)
+				showError('3')
 			 return
 			end
 	end
@@ -85,17 +88,17 @@ local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 					if not name or not retAdr then break end
 				a[i] = {}
 				a[i].Id = i
-				a[i].Adress = Adr
+				a[i].Address = Adr
 				a[i].Name = name
 				i = i + 1
 		end
 			local _, id = m_simpleTV.OSD.ShowSelect_UTF8('Выбрать сезон - ' .. title, 0, a, 5000, 1)
 			if not id then id = 1 end
 			nameses = '  ' .. a[id].Name
-			rc, answer = m_simpleTV.Http.Request(session, {url = a[id].Adress})
+			rc, answer = m_simpleTV.Http.Request(session, {url = a[id].Address})
 				if rc ~= 200 then
 					m_simpleTV.Http.Close(session)
-					m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[3]-' .. rc, 255, 5)
+					showError('4')
 				 return
 				end
 		end
@@ -103,29 +106,29 @@ local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 		local lic = answer:match('<li class="item".-</span>')
 			if not lic then
 				m_simpleTV.Http.Close(session)
-				m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[4]', 255, 5)
+				showError('5')
 			 return
 			end
 		for dataid, name in answer:gmatch('<li class="item".-data%-id="(.-)".-entity%-episode%-name">(.-)</span>') do
 			t[i] = {}
 			t[i].Id = i
 			t[i].Name = FixSpaces(unescape_html(name))
-			t[i].Adress = host .. '/api/v1/video/' .. dataid
+			t[i].Address = host .. '/api/v1/video/' .. dataid
 			i = i + 1
 		end
 		ZonamobiTable = t
 		if i > 2 then
  		local _, id = m_simpleTV.OSD.ShowSelect_UTF8(title .. nameses, 0, t, 5000, 64)
 			if not id then id = 1 end
-			retAdr = t[id].Adress
+			retAdr = t[id].Address
 			title = title .. ' - ' .. t[1].Name
 		else
-			retAdr = t[1].Adress
+			retAdr = t[1].Address
 		end
 		rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
 			if rc ~= 200 then
 				m_simpleTV.Http.Close(session)
-				m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[5]-' .. rc, 255, 5)
+				showError('6')
 			 return
 			end
 	end
@@ -136,13 +139,13 @@ local qlty = 0 -- качество: 0 - низкое; 1 - высокое
 		retAdr = answer:match('"lqUrl":"(.-)"') or answer:match('"url":"(.-)"')
 	end
 		if not retAdr then
-			m_simpleTV.OSD.ShowMessage_UTF8('zonamobi ошибка[6]', 255, 5)
+			showError('7')
 		 return
 		end
 	retAdr = retAdr:gsub('\\/', '/')
 	if m_simpleTV.Control.CurrentTitle_UTF8 then
 		m_simpleTV.Control.CurrentTitle_UTF8 = title
 	end
-	m_simpleTV.OSD.ShowMessageT({text = title, color = ARGB(255, 155, 155, 255), showTime = 1000 * 5, id = "channelName"})
-	m_simpleTV.Control.CurrentAdress = retAdr .. '$OPT:NO-STIMESHIFT'
+	m_simpleTV.OSD.ShowMessageT({text = title, color = 0xff9999ff, showTime = 1000 * 5, id = "channelName"})
+	m_simpleTV.Control.CurrentAddress = retAdr .. '$OPT:NO-STIMESHIFT'
 -- debug_in_file(retAdr .. '\n')
