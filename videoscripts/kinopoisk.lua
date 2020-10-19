092e10aa33e787d664f599d0ad295f7d4f4139b1
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-20 00:54:19 +0300

    Update kinopoisk.lua

--- videoscripts/kinopoisk.lua
+++ videoscripts/kinopoisk.lua
@@ -1,7 +1,7 @@
 -- видеоскрипт для сайта http://www.kinopoisk.ru (20/10/20)
 -- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 -- ## необходимы скрипты ##
--- wink=vod.lua, yandex-vod.lua, kodik.lua, filmix.lua, videoframe.lua, seasonvar.lua
+-- wink-vod.lua, yandex-vod.lua, kodik.lua, filmix.lua, videoframe.lua, seasonvar.lua
 -- zonamobi.lua, iviru.lua, videocdn.lua, hdvb.lua, collaps.lua, ustore.lua
 -- ## открывает подобные ссылки ##
 -- https://www.kinopoisk.ru/film/336434
