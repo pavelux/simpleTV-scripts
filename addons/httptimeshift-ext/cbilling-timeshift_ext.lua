0089b85b63c48060d7e25109236f5803ab98c523
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-21 13:01:01 +0300

    Update cbilling-timeshift_ext.lua

--- addons/httptimeshift-ext/cbilling-timeshift_ext.lua
+++ addons/httptimeshift-ext/cbilling-timeshift_ext.lua
@@ -1,5 +1,5 @@
--- расширение дополнения httptimeshift - cbilling (28/8/20)
--- Copyright © 2017-2020 Nexterr
+-- расширение дополнения httptimeshift - cbilling (21/10/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 	function httpTimeshift_cbilling(eventType, eventParams)
 		if eventType == 'StartProcessing' then
 			if not eventParams.params
