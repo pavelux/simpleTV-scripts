e4d69b4a58638de3efceae3c5db1fffa7ef438b9
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-21 13:04:35 +0300

    Update limehd-timeshift_ext.lua

--- addons/httptimeshift-ext/limehd-timeshift_ext.lua
+++ addons/httptimeshift-ext/limehd-timeshift_ext.lua
@@ -1,5 +1,5 @@
--- расширение дополнения httptimeshift limeHD (28/8/20)
--- Copyright © 2017-2020 Nexterr
+-- расширение дополнения httptimeshift limeHD (21/10/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 	function httpTimeshift_limehd(eventType, eventParams)
 		if eventType == 'StartProcessing' then
 			if not eventParams.params
