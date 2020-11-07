2a1331c7680d30c2a2d0bf5e1d38d59c402da41f
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-11-07 13:32:53 +0300

    Update corntv.lua

--- videoscripts/corntv.lua
+++ videoscripts/corntv.lua
@@ -1,9 +1,11 @@
--- видеоскрипт для плейлиста "corntv" http://corntv.ru (29/9/20)
--- Copyright © 2017-2020 Nexterr
+-- видеоскрипт для плейлиста "corntv" http://corntv.ru (7/11/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 -- ## необходим ##
 -- скрапер TVS: corntv_pls.lua
+-- видеоскрипт: mediavitrina.lua, russiatv.lua
 -- открывает подобные ссылки:
 -- http://corntv.ru/live-tv/ru-tv.html
+-- http://corntv.ru/live-tv/sts.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://corntv%.ru/live') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -12,14 +14,22 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:81.0) Gecko/20100101 Firefox/81.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:83.0) Gecko/20100101 Firefox/83.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 16000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
+	answer = answer:gsub('%s+', '')
 	local retAdr = answer:match('http[^\'\"<>]+%.m3u8[^<>\'\"]*')
+					or answer:match('http[^\'\"<>]+%.mediavitrina[^<>\'\"]*')
 		if not retAdr then return end
-	retAdr = retAdr .. '$OPT:NO-STIMESHIFT'
+		if retAdr:match('mediavitrina') then
+			m_simpleTV.Control.ChangeAddress = 'No'
+			m_simpleTV.Control.CurrentAddress = retAdr
+			dofile(m_simpleTV.MainScriptDir .. 'user\\video\\video.lua')
+		 return
+		end
+	retAdr = retAdr .. '$OPT:NO-STIMESHIFT$OPT:http-referrer=' .. inAdr
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
