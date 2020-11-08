5657a1df3a9c443ca804de9e84beac7a53f59248
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-11-08 22:54:39 +0300

    Update pokazzTV.lua

--- videoscripts/pokazzTV.lua
+++ videoscripts/pokazzTV.lua
@@ -1,9 +1,10 @@
--- видеоскрипт для плейлиста "pokazzTV" http://pokaz.me (26/8/20)
--- Copyright © 2017-2020 Nexterr
--- необходим скрапер TVS: pokazzTV_pls
+-- видеоскрипт для плейлиста "pokazzTV" http://pokaz.me (8/11/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
+-- ## необходим ##
+-- необходим скрапер TVS: pokazzTV_pls.lua
 -- необходим модуль: /core/playerjs.lua
--- открывает подобные ссылки:
--- http://tv.pokaz.me/466-teatr.html
+-- ## открывает подобные ссылки ##
+-- http://tv.pokaz.me/401-kanal-sony-sci-fi.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://[%a%.]*pokaz%.') then return end
 	require 'playerjs'
@@ -13,23 +14,24 @@
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:80.0) Gecko/20100101 Firefox/80.0')
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:83.0) Gecko/20100101 Firefox/83.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	answer = answer:gsub('<!%-%-.-%-%->', ''):gsub('/%*.-%*/', '')
+	answer = answer:gsub('%s+', '')
 	local retAdr = answer:match('file:"([^"]+)')
 		if not retAdr then return end
-	local playerjs_url = answer:match('src="(/templates/shamanim/js/tv.-)"')
+	local playerjs_url = answer:match('src="(/templates/shamanim/js/tv[^"]+)')
 		if not playerjs_url then return end
 	playerjs_url = inAdr:match('^https?://[^/]+') .. playerjs_url
 	retAdr = playerjs.decode(retAdr, playerjs_url)
 		if not retAdr or retAdr == '' then return end
-	local v1 = answer:match('firstIpProtect = \'([^\']+)') or ''
-	local v2 = answer:match('secondIpProtect = \'([^\']+)') or ''
-	local v3 = answer:match('portProtect = \'([^\']+)') or ''
+	local v1 = answer:match('firstIpProtect=\'([^\']+)') or ''
+	local v2 = answer:match('secondIpProtect=\'([^\']+)') or ''
+	local v3 = answer:match('portProtect=\'([^\']+)') or ''
 	retAdr = retAdr:gsub('{v1}', v1):gsub('{v2}', v2):gsub('{v3}', v3)
 	retAdr = retAdr .. '$OPT:http-referrer=' .. inAdr
 	m_simpleTV.Control.CurrentAddress = retAdr
