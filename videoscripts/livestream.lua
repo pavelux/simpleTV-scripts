0d1eaf811af151dede20b51234e2a6a451d69c21
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-11-26 13:37:03 +0300

    Update livestream.lua

--- videoscripts/livestream.lua
+++ videoscripts/livestream.lua
@@ -1,10 +1,12 @@
 -- видеоскрипт для сайта https://livestream.com (26/11/20)
 -- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
 -- открывает подобные ссылки:
+-- https://livestream.com/accounts/10612724/newstalk57
 -- https://livestream.com/accounts/9869799/events/3519786
 -- https://livestream.com/accounts/21927570/events/7222857/videos/182731354
 -- https://livestream.com/accounts/29119579/nchafuturity2020
 -- https://livestream.com/calciocataniachannel/events/9333273/videos/211648595
+-- https://livestream.com/accounts/362/events/3557232/videos/67864563/player?autoPlay=false&height=360&mute=false
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://livestream%.com') then return end
 	if m_simpleTV.Control.MainMode == 0 then
@@ -15,24 +17,17 @@
 	m_simpleTV.Control.CurrentAddress = 'error'
 	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:84.0) Gecko/20100101 Firefox/84.0')
 		if not session then return end
-	m_simpleTV.Http.SetTimeout(session, 8000)
-	if not inAdr:match('/accounts/(%d+)') then
-		local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
-			if rc ~= 200 then return end
-		local accounts = answer:match('/accounts/%d+/')
-		if accounts then
-			inAdr = inAdr:gsub('^.-/(events/.-)$', 'https://livestream.com' .. accounts .. '%1')
-		end
-	end
-	local rc, answer = m_simpleTV.Http.Request(session, {url = 'https://livestream.com/oembed?url=' .. inAdr})
-		if rc ~= 200 and not inAdr:match('/events/(%d+)') then return end
-	local thumb = answer:match('"thumbnail_url":"([^"]+)')
-	inAdr = answer:match('src=\\"(.-)/player') or inAdr
-	inAdr = inAdr:gsub('^https?://', 'https://api.new.')
+	m_simpleTV.Http.SetTimeout(session, 12000)
+	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
+		if rc ~= 200 then return end
+	local accounts_events = answer:match('/accounts/%d+/events/%d+')
+		if not accounts_events then return end
+	local videos = inAdr:match('/videos/%d+') or ''
+	inAdr = 'https://api.new.livestream.com' .. accounts_events .. videos
 	rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
-	local retAdr
+	local retAdr, title
 	answer = answer:gsub('\\"', '%%22')
 	if inAdr:match('/videos/(%d+)') then
 		retAdr = answer:match('"secure_m3u8_url":"([^"]+)')
@@ -44,9 +39,9 @@
 		if not retAdr then return end
 	title = title or 'livestream'
 	title = unescape3(title)
-	thumb = answer:match('"logo".-"url":"([^"]+)')
-			or thumb
-			or 'https://cdn.livestream.com/deploy/website/production/4827e61/assets/m/icon-iphone@2x.png'
+	local thumb = answer:match('"logo".-"url":"([^"]+)')
+				or answer:match('"comments".-"thumbnail_url_small":"([^"]+)')
+				or 'https://cdn.livestream.com/deploy/website/production/4827e61/assets/m/icon-iphone@2x.png'
 	m_simpleTV.Control.CurrentTitle_UTF8 = title
 	m_simpleTV.Control.ChangeChannelLogo(thumb, m_simpleTV.Control.ChannelID, 'CHANGE_IF_NOT_EQUAL')
 	retAdr = retAdr:gsub('%.smil', '.m3u8')
