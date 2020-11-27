2038e53dcbacbda0390a966444778c467004804e
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-11-27 03:51:49 +0300

    Update poisk_acestream.lua

--- videoscripts/poisk_acestream.lua
+++ videoscripts/poisk_acestream.lua
@@ -1,16 +1,19 @@
--- видеоскрипт для поиска трансляций ACEStream http://acestream.org (8/6/19)
--- необходим Acestream
+-- видеоскрипт для поиска трансляций AceStream http://acestream.org (27/11/20)
+-- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr/simpleTV
+-- необходим устаовленный Acestream
 -- искать через команду меню "Открыть URL" (Ctrl+N)
 -- использовать префикс "+"
 -- открывает подобные запросы: + матч
-		if m_simpleTV.Control.ChangeAdress ~= 'No' then return end
-	local inAdr = m_simpleTV.Control.CurrentAdress
-		if not inAdr then return end
-		if not inAdr:match('^%+') then return end
-	m_simpleTV.OSD.ShowMessageT({text = '', color = ARGB(0, 0, 0, 0), showTime = 1000 * 1, id = 'channelName'})
-	m_simpleTV.Control.ChangeAdress = 'Yes'
-	m_simpleTV.Control.CurrentAdress = ''
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/73.0.2785.143 Safari/537.36')
+		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^%+') then return end
+	m_simpleTV.OSD.ShowMessageT({text = '', showTime = 1000, id = 'channelName'})
+	local inAdr = m_simpleTV.Control.CurrentAddress
+	local function showError(str)
+		m_simpleTV.OSD.ShowMessageT({text = 'поиск AceStream ошибка: ' .. str, showTime = 5000, color = 0xffff6600, id = 'channelName'})
+	end
+	m_simpleTV.Control.ChangeAddress = 'Yes'
+	m_simpleTV.Control.CurrentAddress = ''
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:84.0) Gecko/20100101 Firefox/84.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 12000)
 	inAdr = inAdr:gsub('^%+', '')
@@ -19,14 +22,14 @@
 	local rc, answer = m_simpleTV.Http.Request(session, {url = url})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then
-			m_simpleTV.OSD.ShowMessageT({text = 'poisk_acestream ошибка[1]-' .. rc, color = ARGB(255, 255, 0, 0), showTime = 1000 * 5, id = 'channelName'})
+			showError('[1]')
 		 return
 		end
-	answer = answer:gsub('(%[%])', '"nil"'):gsub(string.char(239, 187, 191), ''):gsub('\\', '\\\\'):gsub('\\"', '\\\\"'):gsub('\\/', '/')
+	answer = answer:gsub('%[%]', '""'):gsub(string.char(239, 187, 191), ''):gsub('\\', '\\\\'):gsub('\\"', '\\\\"'):gsub('\\/', '/')
 	require 'json'
 	local tab = json.decode(answer)
 		if not tab or not tab.results then
-			m_simpleTV.OSD.ShowMessageT({text = 'poisk_acestream ошибка[2]', color = ARGB(255, 255, 0, 0), showTime = 1000 * 5, id = 'channelName'})
+			showError('[2]')
 		 return
 		end
 	local t, i, h = {}, 1, 1
@@ -37,20 +40,21 @@
 			then
 				t[i] = {}
 				t[i].Id = i
-				t[i].Name = unescape3(tab.results[h].name)
-				t[i].Adress = 'torrent://INFOHASH=' .. tab.results[h].infohash
+				local name = tab.results[h].name:gsub('\\"', '"')
+				t[i].Name = unescape3(name)
+				t[i].Address = 'torrent://INFOHASH=' .. tab.results[h].infohash
 				i = i + 1
 			end
 			h = h + 1
 		end
-		if i == 1 then
-			m_simpleTV.OSD.ShowMessageT({text = 'не найдено\n\npoisk_acestream ошибка[3]', color = ARGB(255, 255, 0, 0), showTime = 1000 * 5, id = 'channelName'})
+		if #t == 0 then
+			showError('не найдено')
 		 return
 		end
 	m_simpleTV.Control.ExecuteAction(11)
 	local ret, id = m_simpleTV.OSD.ShowSelect_UTF8(inAdr .. ' - поиск ACEStream', 0, t, 0)
 		if not id then return end
 	if ret == 1 then
-		m_simpleTV.Control.CurrentAdress = t[id].Adress
+		m_simpleTV.Control.CurrentAddress = t[id].Address
 	end
--- debug_in_file(t[id].Adress .. '\n')
+-- debug_in_file(t[id].Address .. '\n')
